1358aa4e48c2296079c68d12e8e5574bd4a259ee
cossio <cossio@users.noreply.github.com>
2026-03-01 16:49:27 +0100

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-import Aqua, MakieSequenceLogos
+import Aqua, SequenceLogos
 using Test: @testset
 
 @testset "aqua" begin
-    Aqua.test_all(MakieSequenceLogos)
+    Aqua.test_all(SequenceLogos)
 end
